d8c432a44f86aedc2f298fe96fadc59eddb52b42
bar oren <bar.oren10@gmail.com>
2021-03-01 10:37:14 +0200

    initial commit

---
+++ git test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git test
+//
+//  Created by Bar Oren on 01/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "test\n";
+    return 0;
+}
